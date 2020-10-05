2f0222d9b8f836e6c78dbc08334538836b58fc10
Kethmi1999 <68540579+Kethmi1999@users.noreply.github.com>
2020-10-05 11:34:14 +0530

    Kethmi initial commit

---
+++ Grocerryshopper-master/app/src/main/java/com/example/grocerryshopper/ReadyOrder.java
@@ -0,0 +1,14 @@
+package com.example.grocerryshopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadyOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ready_order);
+    }
+}
